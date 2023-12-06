ad864c4019ae5ba75592359826a200057d936366
haraphat01 <haraphat@gmail.com>
2023-12-06 23:06:17 +0100

    Added the svg and restyled the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
 import Image from 'next/image'
 import Navigation from './components/Navigation'
 import Footer from './components/Footer'
+import div1 from "../../public/images/hero.png"
 
 export default function Home() {
   return (
     <main className="min-h-screen  ">
       <div className="bg-black text-white">
         <Navigation />
-        <div className="p-4 ">
-          <div className=' mt-5 h-80 text-center'>
-            <p className='text-5xl mb-5'>Welcome to Bizlink  </p>
-            <p className='text-3xl mb-5'>where small business dreams meet their perfect match</p>
-            <p className='text-3xl mb-5'>Whether you're a passionate entrepreneur looking to sell your thriving business or a savvy investor seeking the next big opportunity, BizLink is the bridge that brings your goals to reality.</p>
-          </div>
-          <div className="my-4">
-            <h2>Key Benefits:</h2>
+        <div className="p-4 text-center">
+        <p className='text-5xl mb-5'>Welcome to Bizlink  </p>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-5 text-center">
+  <div className="md:order-2">
+    <p className='text-3xl mb-5'>Where small business dreams meet their perfect match</p>
+    <p className='text-3xl '>Whether you're a passionate entrepreneur looking to sell your thriving business or a savvy investor seeking the next big opportunity, BizLink is the bridge that brings your goals to reality.</p>
+  </div>
+  <div className="md:order-1 w-full md:w-1/2 flex justify-center items-center">
+    <Image src={div1} alt="description of image" className=" object-cover" />
+  </div>
+</div>
+          <div className=" text-center mt-5">
+            <h2 className='text-5xl'>Key Benefits:</h2>
             <ul>
               <li>
                 Simplify the Selling Process: Effortlessly list your business and connect with qualified buyers in just a few clicks.
@@ -24,10 +30,11 @@ export default function Home() {
             </ul>
           </div>
 
-          <div className="my-4">
-            <h2>Get Started Today:</h2>
-            <p>Ready to embark on your business journey? Join BizLink now and take the first step towards a successful business transaction.</p>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-md">List your business</button>
+          <div className="text-center mt-5">
+            <h2 className='text-5xl'>Get Started Today:</h2>
+            <p>Ready to embark on your business journey? </p>
+            <p>Join BizLink now and take the first step towards a successful business transaction</p>
+            <button className="bg-blue-800 text-white px-4 py-2 rounded-md">List your business</button>
           </div>
         </div>
         <Footer />
