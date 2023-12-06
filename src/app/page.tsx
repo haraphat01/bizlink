e5f2e24135c9aace9f7ec30c2a1e3f12bdb7244b
haraphat01 <haraphat@gmail.com>
2023-12-06 23:32:27 +0100

    Added little animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <Navigation />
         <div className="p-4 text-center">
         <p className='text-5xl mb-5 animate-pulse text-red-200'>Welcome to Bizlink</p>
-          <div className="flex flex-col md:flex-row justify-center items-center mt-5 text-center">
+          <div className="flex flex-col md:flex-row justify-center items-center">
             <div className="md:order-2">
               <p className='text-2xl mb-5'>Where small business dreams meet their perfect match</p>
               <p className='text-2xl '>Whether you're a passionate entrepreneur looking to sell your thriving business or a savvy investor seeking the next big opportunity, BizLink is the bridge that brings your goals to reality.</p>
@@ -27,7 +27,7 @@ export default function Home() {
               <li className="mb-3 text-2xl">Trust in the accuracy of information with our verification process, ensuring that each listing reflects the true potential of the business.</li>
             </ul>
           </div>
-          <div className="text-center mt-5">
+          <div className="text-center mb-5 mt-5">
             <h2 className='text-5xl animate-pulse text-red-200'>Get Started Today:</h2>
             <p className="mb-3 text-2xl">Ready to embark on your business journey? </p>
             <p className="mb-3 text-2xl">Join BizLink now and take the first step towards a successful business transaction</p>
