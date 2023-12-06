5ff665354bdff4325655d241615db4e22868b92c
haraphat01 <haraphat@gmail.com>
2023-12-06 23:26:01 +0100

    Added little animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,31 +9,28 @@ export default function Home() {
       <div className="bg-black text-white">
         <Navigation />
         <div className="p-4 text-center">
-        <p className='text-5xl mb-5'>Welcome to Bizlink  </p>
-        <div className="flex flex-col md:flex-row justify-center items-center mt-5 text-center">
-  <div className="md:order-2">
-    <p className='text-3xl mb-5'>Where small business dreams meet their perfect match</p>
-    <p className='text-3xl '>Whether you're a passionate entrepreneur looking to sell your thriving business or a savvy investor seeking the next big opportunity, BizLink is the bridge that brings your goals to reality.</p>
-  </div>
-  <div className="md:order-1 w-full md:w-1/2 flex justify-center items-center">
-    <Image src={div1} alt="description of image" className=" object-cover" />
-  </div>
-</div>
-          <div className=" text-center mt-5">
-            <h2 className='text-5xl'>Key Benefits:</h2>
-            <ul>
-              <li>
-                Simplify the Selling Process: Effortlessly list your business and connect with qualified buyers in just a few clicks.
-              </li>
-              <li>Direct Communication: Seamlessly communicate with sellers or buyers through our secure messaging platform, facilitating transparent and efficient negotiations.</li>
-              <li>Verified Listings: Trust in the accuracy of information with our verification process, ensuring that each listing reflects the true potential of the business.</li>
+        <p className='text-5xl mb-5 animate-pulse text-red-200'>Welcome to Bizlink</p>
+          <div className="flex flex-col md:flex-row justify-center items-center mt-5 text-center">
+            <div className="md:order-2">
+              <p className='text-2xl mb-5'>Where small business dreams meet their perfect match</p>
+              <p className='text-2xl '>Whether you're a passionate entrepreneur looking to sell your thriving business or a savvy investor seeking the next big opportunity, BizLink is the bridge that brings your goals to reality.</p>
+            </div>
+            <div className="md:order-1 w-full md:w-1/2 flex justify-center items-center">
+              <Image src={div1} alt="description of image" className=" object-cover" />
+            </div>
+          </div>
+          <div className="text-center mt-5">
+            <h2 className='text-5xl animate-pulse text-red-200'>Key Benefits:</h2>
+            <ul className="">
+              <li className="mb-3 text-2xl"> Effortlessly list your business and connect with qualified buyers in just a few clicks.</li>
+              <li className="mb-3 text-2xl">Seamlessly communicate with sellers or buyers through our secure messaging platform, facilitating transparent and efficient negotiations.</li>
+              <li className="mb-3 text-2xl">Trust in the accuracy of information with our verification process, ensuring that each listing reflects the true potential of the business.</li>
             </ul>
           </div>
-
           <div className="text-center mt-5">
-            <h2 className='text-5xl'>Get Started Today:</h2>
-            <p>Ready to embark on your business journey? </p>
-            <p>Join BizLink now and take the first step towards a successful business transaction</p>
+            <h2 className='text-5xl animate-pulse text-red-200'>Get Started Today:</h2>
+            <p className="mb-3 text-2xl">Ready to embark on your business journey? </p>
+            <p className="mb-3 text-2xl">Join BizLink now and take the first step towards a successful business transaction</p>
             <button className="bg-blue-800 text-white px-4 py-2 rounded-md">List your business</button>
           </div>
         </div>
